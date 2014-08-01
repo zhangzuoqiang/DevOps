0b9024b55638a0c7cd4e61558804a078c8f04e58
hzchenkj <hzchenkj@gmail.com>
2014-08-01 23:30:42 +0800

    增加lvs 安装 nat ,iptun ,dr add nginx conf comment

--- lvs/lvs_install.sh
+++ lvs/lvs_install.sh
@@ -39,3 +39,103 @@ ipvsadm -C
 ipvsadm -A -t 192.168.32.150:80 -s rr -p 600
 ipvsadm -a -t 192.168.32.150:80 -r 192.168.32.149:80 -g
 ipvsadm -a -t 192.168.32.150:80 -r 192.168.32.151:80 -g
+
+
+
+1. lvs-nat:
+检查三台服务器防火墙
+iptables -L -n
+sestatus
+
+ds: 2网卡
+echo 1 > /proc/sys/net/ipv4/ip_forward
+ifconfig eth0 192.168.32.147 netmask 255.255.255.0
+ifconfig eth1 192.168.32.150 netmask 255.255.255.0
+route add default gw 192.168.32.2
+route -n
+
+cat >> lvs-nat.sh <<EOF
+ipvsadm -C
+ipvsadm -At 192.168.32.150:80 -s rr
+ipvsadm -at 192.168.32.150:80 -r 192.168.32.149:80 -m
+ipvsadm -at 192.168.32.150:80 -r 192.168.32.151:80 -m
+ipvsadm
+EOF
+chmod +x lvs-nat.sh
+./lvs-nat.sh
+
+rs1:
+ifconfig eth1 192.168.10.2 netmask 255.255.255.0
+route add default gw 192.168.32.2
+route -n
+rs2:
+ifconfig eth1 192.168.10.3 netmask 255.255.255.0
+route add default gw 192.168.32.2
+route -n
+
+2. lvs-ip tun:
+ifconfig eth0 200.168.10.1 netmask 255.255.255.0
+ifconfig tunl0 200.168.10.10 netmask 255.255.255.255 up
+route add -host 200.168.10.10 dev   tunl0
+
+cat >> lvs-tun.sh <<EOF
+ipvsadm -C
+ipvsadm -At 200.168.10.10:80 -s rr
+ipvsadm -at 200.168.10.10:80 -r 200.168.10.2:80 -i
+ipvsadm -at 200.168.10.10:80 -r 200.168.10.3:80 -i
+ipvsadm
+EOF
+
+rs1:
+ifconfig eth0 200.168.10.2 netmask 255.255.255.0
+ifconfig tunl0 200.168.10.10 netmask 255.255.255.255 up
+route add -host 200.168.10.10 dev tunl0
+echo "1" > /proc/sys/net/ipv4/conf/tunl0/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/tunl0/arp_announce
+echo "1" > /proc/sys/net/ipv4/conf/all/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/all/arp_announce
+
+
+rs2:
+ifconfig eth0 200.168.10.3 netmask 255.255.255.0
+ifconfig tunl0 200.168.10.10 netmask 255.255.255.255 up
+route add -host 200.168.10.10 dev tunl0
+echo "1" > /proc/sys/net/ipv4/conf/tunl0/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/tunl0/arp_announce
+echo "1" > /proc/sys/net/ipv4/conf/all/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/all/arp_announce
+
+2. lvs-dr tun:
+ifconfig eth0:0 200.168.10.10 netmask 255.255.255.255
+route add -host 200.168.10.10 dev eht0:0
+ifconfig tunl0 down
+
+cat >> lvs-dr.sh <<EOF
+ipvsadm -C
+ipvsadm -At 200.168.10.10:80 -s rr
+ipvsadm -at 200.168.10.10:80 -r 200.168.10.2:80 -g
+ipvsadm -at 200.168.10.10:80 -r 200.168.10.3:80 -g
+ipvsadm
+EOF
+
+
+rs1:
+ifconfig eth0 200.168.10.2 netmask 255.255.255.0
+ifconfig lo:0 200.168.10.10 netmask 255.255.255.255 up
+route add -host 200.168.10.10 dev lo:0
+echo "1" > /proc/sys/net/ipv4/conf/lo/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/lo/arp_announce
+echo "1" > /proc/sys/net/ipv4/conf/all/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/all/arp_announce
+ifconfig tunl0 down
+
+rs2:
+ifconfig eth0 200.168.10.3 netmask 255.255.255.0
+ifconfig lo:0 200.168.10.10 netmask 255.255.255.255 up
+route add -host 200.168.10.10 dev lo:0
+echo "1" > /proc/sys/net/ipv4/conf/lo/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/lo/arp_announce
+echo "1" > /proc/sys/net/ipv4/conf/all/arp_ignore
+echo "2" > /proc/sys/net/ipv4/conf/all/arp_announce
+ifconfig tunl0 down
+
