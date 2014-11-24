0aaacc68031285bd6c42f286f18b56c9c2ad002a
hzchenkj <hzchenkj@gmail.com>
2014-11-24 10:39:14 +0800

    add python mail smtp image mime

---
+++ python/mail.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import smtplib
+import string
+
+HOST = "mail.winupon.com"
+SUBJECT ="Test email from python smtplib"
+TO = "hzchenkj@163.com"
+FROM = "chenkj@winupon.com" 
+text = "Python rules them all!"
+BODY = string.join((
+		"From: %s" % FROM ,
+		"To: %s" % TO,
+		"Subject: %s" % SUBJECT,
+		"",
+		text 
+	),"\r\n")
+server = smtplib.SMTP()
+server.connect(HOST,"25")
+server.starttls()
+server.login("chenkj@winupon.com","86256876")
+server.sendmail(FROM,[TO],BODY)
+server.quit()
